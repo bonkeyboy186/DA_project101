4f3012873ec455fd90fc4b3f6079fbbd44cf5d46
K-XY <98725521+K-XY@users.noreply.github.com>
2022-02-27 22:17:13 +0800

    Update loadconnect.py

--- loadconnect.py
+++ loadconnect.py
@@ -12,7 +12,7 @@ except socket.error as err:
 # Default web port
 port = 80;
 try:
-    host_ip = socket.gethostbyname("http://brickset.com/sets/2019");
+    host_ip = socket.gethostbyname("http://192.168.17.84/freebix");
 except socket.gaierror:
     print("There is a problem resolving the host");
     sys.exit();
